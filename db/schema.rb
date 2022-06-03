df1d54b943dd01f41864d818a5519e3d3f84de54
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-02 22:40:48 -0300

    creating shoe entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_001909) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_002134) do
+  create_table "shoes", force: :cascade do |t|
+    t.string "model"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
