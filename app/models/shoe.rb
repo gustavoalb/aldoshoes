d9a568727249456764ba971e7262aaf7ad058be8
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-05 17:14:53 -0300

    Adding uniqueness to shoe and store

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,6 @@
 class Shoe < ApplicationRecord
     has_many :inventory_items
     has_many :stores, through: :inventory_items
+    
+    validates_uniqueness_of :model
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,6 @@
 class Store < ApplicationRecord
     has_many :inventory_items
     has_many :shoes, through: :inventory_items
+
+    validates_uniqueness_of :name
 end
