25cb88d969ed1ad27866b327e8cf70bda905bc99
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-05 01:58:47 -0300

    adding associations to shoe & store

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,4 @@
 class Shoe < ApplicationRecord
+    has_many :inventory_items
+    has_many :stores, through: :inventory_items
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,4 @@
 class Store < ApplicationRecord
+    has_many :inventory_items
+    has_many :shoes, through: :inventory_items
 end
