c9ca48bf6eb7fe92b83607365a719493f995b7a2
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-06 03:13:08 -0300

    Add graphql to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
 end
