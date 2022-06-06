b089f5ff33c6792d7c2fb4f9d1c2667f19f62b50
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-06 15:58:17 -0300

    Delete unused methods

--- app/graphql/aldos_shoes_schema.rb
+++ app/graphql/aldos_shoes_schema.rb
@@ -1,18 +1,11 @@
+# frozen_string_literal: true
+
 class AldosShoesSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
   use GraphQL::Dataloader
-
-  # GraphQL-Ruby calls this when something goes wrong while running a query:
-  def self.type_error(err, context)
-    # if err.is_a?(GraphQL::InvalidNullError)
-    #   # report to your bug tracker here
-    #   return nil
-    # end
-    super
-  end
 
   # Relay-style Object Identification:
 
