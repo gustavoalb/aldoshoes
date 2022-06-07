8c5f20539dfb3af0e1cc0f4ce077b85afef93690
Gustavo Albuquerque <gustavo.albuquerque@fcamara.com.br>
2022-06-07 00:46:50 -0300

    correcting rubocop offenses

--- spec/factories/shoes.rb
+++ spec/factories/shoes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :shoe do
     model { "#{Faker::Game.title} Sneakers #{rand(1..10)}" }
--- spec/factories/stores.rb
+++ spec/factories/stores.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :store do
     name { "#{Faker::Game.title} Store #{rand(1..10)}" }
